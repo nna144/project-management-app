fce46a5ababf16854d66adbead0170f9dc765901
nna144 <llina.tvo@gmail.com>
2020-04-25 19:45:58 +0200

    Connected to the redux store

--- src/store/reducers/projectReducer.js
+++ src/store/reducers/projectReducer.js
@@ -1,4 +1,10 @@
-const initialState = {};
+const initialState = {
+  projects: [
+    { id: "1", title: "Collect all the stars", content: "la la la" },
+    { id: "2", title: "Print the pages", content: "la la la" },
+    { id: "3", title: "Fix the navbar", content: "la la la" },
+  ],
+};
 
 const projectReducer = (state = initialState, action) => {
   return state;
