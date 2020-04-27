d6c9345282ea7d9b56986b55e1180987732dc9ad
nna144 <llina.tvo@gmail.com>
2020-04-27 20:22:00 +0200

    Set up Firebase

---
+++ src/config/fbConfig.js
@@ -0,0 +1,21 @@
+import firebase from "firebase/app";
+import "firebase/firestore";
+import "firebase/auth";
+
+// Your web app's Firebase configuration
+var firebaseConfig = {
+  apiKey: "AIzaSyCE8vw-Jbot863fVJOI56gkCm5QIOnYNnM",
+  authDomain: "project-mangement-app.firebaseapp.com",
+  databaseURL: "https://project-mangement-app.firebaseio.com",
+  projectId: "project-mangement-app",
+  storageBucket: "project-mangement-app.appspot.com",
+  messagingSenderId: "533493678281",
+  appId: "1:533493678281:web:7873a8f5e54eb4f6510d18",
+  measurementId: "G-F92EQY7SM8",
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
+firebase.firestore().settings({ timestampsInSnapshots: true });
+
+export default firebase;
